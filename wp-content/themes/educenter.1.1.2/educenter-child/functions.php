48f9258d4551698b313f780c5c55516a56e3dd1d
EC2 Default User <ec2-user@ip-172-31-36-226.us-east-2.compute.internal>
2019-10-09 02:46:08 +0000

    Beginning work on Mentor page, and matching mentees and mentors

--- wp-content/themes/educenter.1.1.2/educenter-child/functions.php
+++ wp-content/themes/educenter.1.1.2/educenter-child/functions.php
@@ -14,7 +14,10 @@ class EDUCENTER_CHILD_THEME {
     public function __construct() {
 
         // Enqueue scripts/styles
-        add_action ( 'wp_enqueue_scripts', array ( $this, 'wp_enqueue_scripts' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+
+        // Add a new tab to the profile
+        add_filter( 'um_after_header_meta', array( $this, 'um_after_header_meta' ), 10, 2 );
     }
 
     //Enqueue scripts and styles
@@ -37,6 +40,28 @@ class EDUCENTER_CHILD_THEME {
         );
 
     }
+
+    public function um_after_header_meta( $user_id, $args ) {
+
+        // Get the mentor role for the user of the profile being viewed
+        $profile_user_mentor_role = get_user_meta( $user_id, 'mentor_role', true);
+
+        // Get the mentor role for the logged in user
+        $current_user_id = get_current_user_id();
+        $current_user_mentor_role = get_user_meta( $current_user_id, 'mentor_role', true);
+
+        // If the profile user is a mentor, the currently logged in user is a mentee, and the profile being viewed isn't the logged in users profile, show the request mentor button
+        if( $profile_user_mentor_role == 'Mentor' && $current_user_mentor_role == 'Mentee' && $user_id != $current_user_id ) {
+
+            ?>
+
+            <button class="request_mentorship" name="request_mentorship" id="request_mentorship" >Request Mentorship</button>
+
+            <?php
+
+        }
+    }
+
 }
 $EDUCENTER_CHILD_THEME = new EDUCENTER_CHILD_THEME();
 
--- wp-content/themes/educenter.1.1.2/educenter-child/shortcodes/shortcodes.php
+++ wp-content/themes/educenter.1.1.2/educenter-child/shortcodes/shortcodes.php
@@ -9,6 +9,8 @@ class Barkers_Boys_Shortcodes {
     public function __construct() {
 
         add_shortcode( 'home_page', array( $this, 'home_page') );
+
+        add_shortcode( 'mentor_matchmaker', array( $this, 'mentor_matchmaker' ) );
     }
 
     public function home_page() {
@@ -29,22 +31,92 @@ class Barkers_Boys_Shortcodes {
             </div>
             <div class="col-md-4" >
                 <div class="about-title" >
-                <img class="cardinal" src="<?php  echo get_stylesheet_directory_uri() . '/assets/images/cardinal.png' ?>" />
-                <h2>Cards Only</h2>
-                <p>Have the comfort of knowing you're getting help you can trust!  All members of Cardinal Counsel are current students or alumni of the University of Louisville!  
-                All user sign ups are reviewed by a member of the U of L faculty before the are given access to the site.</p>
+                    <img class="cardinal" src="<?php  echo get_stylesheet_directory_uri() . '/assets/images/cardinal.png' ?>" />
+                    <h2>Cards Only</h2>
+                    <p>Have the comfort of knowing you're getting help you can trust!  All members of Cardinal Counsel are current students or alumni of the University of Louisville!  
+                    All user sign ups are reviewed by a member of the U of L faculty before the are given access to the site.</p>
                 </div>
             </div>
             <div class="col-md-4" >
                 <div class="about-title" >
-                <i class="fas fa-hands-helping"></i>
-                <h2>Mentor Matchmaker</h2>
-                <p>Prefer one on one help?  Use our custom built Mentor Matchmaker!  The Mentor Matchmaker will attempt to match you with a fellow cardinal who is best equipped to help you be successful!</p>
+                    <i class="fas fa-hands-helping"></i>
+                    <h2>Mentor Matchmaker</h2>
+                    <p>Prefer one on one help?  Use our custom built Mentor Matchmaker!  The Mentor Matchmaker will attempt to match you with a fellow cardinal who is best equipped to help you be successful!</p>
                 </div>
             </div>
         </div>
 
         <?php
+    }
+
+    public function mentor_matchmaker() {
+       
+        // Get the current user ID
+        $user_id = get_current_user_id();
+
+        // Get the mentor role.  Should be Mentor, Mentee, or Neither.
+        $mentor_role = get_user_meta( $user_id, 'mentor_role', true );
+
+        if( $mentor_role == 'Mentor' ) {
+            
+            ?>
+
+            <div class="row mentor-matchmaker" >
+                <div class="col-md-12" >
+                    <h4>My Mentee(s)</h4>
+                </div>
+                <div class="col-md-12" >
+                </div>
+                <div class="col-md-12" >
+                    <h4>My Requests</h4>
+                </div>
+                <div class="col-md-12" >
+                </div>
+            </div>
+
+            <?php
+        }
+
+        if( $mentor_role== 'Mentee' ) {
+            
+            ?>
+
+            <div class="row mentor-matchmaker" >
+                <div class="col-md-12" >
+                    <h4>My Mentor</h4>
+                </div>
+                <div class="col-md-12" >
+                </div>
+                <div class="col-md-9" >
+                    <h4>My Top 5 Mentor Matches</h4>
+                </div>
+                <div class="col-md-3" >
+                    <button class="all_mentors" name="all_mentors" id="all_mentors" >All Mentors</button>
+                </div>
+                <div class="col-md-12" >
+                </div>
+            </div>
+
+            <?php
+        }
+
+        if( $mentor_role == "Neither" || empty( $mentor_role) ) {
+
+            ?>
+
+            <div class="row mentor-matchmaker" >
+                <div class="col-md-12" >
+                    <h4>Welcome to the Mentor MatchMaker!</h4>
+                </div>
+                <div class="col-md-12" >
+                    <p>The Mentor MatchMaker is our own custom tool built to match you with a fellow cardinal who is best equipped to help you be successful! To get started, select your mentor role
+                    from the <a class="edit_profile" href="<?php echo um_user_profile_url(); ?>?profiletab=main&um_action=edit&um_action=edit" >edit profile</a> page </p>
+                </div>
+            </div>
+
+            <?php
+        }
+
     }
 }
 $Barkers_Boys_Shortcodes = new Barkers_Boys_Shortcodes();
